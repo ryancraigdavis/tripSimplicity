90c67c94f9e3ca329cccfa500845763c0472aab2
Ryan Davis <ryancraigdavis@gmail.com>
2015-10-25 18:56:38 -0700

    api fixes

--- public/scripts/hotelAirService.js
+++ public/scripts/hotelAirService.js
@@ -52,7 +52,7 @@ var app = angular.module('travelApp');
 			var deferred = $q.defer();
 			$http({
 				method: 'POST', 
-				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAIVZERszQwnKfoKC9niU55SCTXFUqaEaQ',
+				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAIVZERszQwnKfoKC9niU55SCTXFUqaE',
 				data: {
 				  "request": {
 				    "slice": [
